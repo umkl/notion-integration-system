a50afee8bda5770d58ac99b1229584c0c7ed9163
ungarmichael <ungarmichael.mail@gmail.com>
2021-09-09 10:25:18 +0200

    fix: folder structure issues

--- NIS-Nodejs/src/cli.js
+++
@@ -1,9 +0,0 @@
-import arg from 'arg';
-
-function parseArgumentsIntoOptions(rawArgs){
-    const args = arg()
-}
-
-export function cli(args){
-    console.log(args);
-}
--- NIS-Nodejs/src/index.ts
+++ NIS-Nodejs/src/index.ts
@@ -1,4 +1,4 @@
-//IMPORTS 
+//IMPORTS
 import { GoogleCalendarIntegration } from "./repository/GoogleCloud/GoogleCalendar";
 import { NotionIntegration } from "./repository/NotionIntegrations/Notion";
 // import { Action } from "./types/nis";
@@ -12,35 +12,29 @@ const app = express();
 const port = 4000;
 
 //declaring variables
-var ni: NotionIntegration;
-var gci: GoogleCalendarIntegration;
-var nNotion: Action[];
-var cNotion: Action[];
-var idCNotion: string[];
-var idCGoogleCalendar: string[];
-var cGoogleCalendar: Action[];
-var nGoogleCalendar: Action[];
-var notionChBuffer: Action[];
-
+// var ni: NotionIntegration;
+// var gci: GoogleCalendarIntegration;
+// var nNotion: Action[];
+// var cNotion: Action[];
+// var idCNotion: string[];
+// var idCGoogleCalendar: string[];
+// var cGoogleCalendar: Action[];
+// var nGoogleCalendar: Action[];
+// var notionChBuffer: Action[];
 
 serverPreparation();
 startServer();
 
 // app.listen(port, async () => {
-  // serverPreparation();
-  // startServer();
+// serverPreparation();
+// startServer();
 // });a
 
 // console.log("hello")
 
-function main() {
-  
-}
+function main() {}
 
 main();
-
-
-
 
 function applyNNotionToCGoogleCalendar() {
   generateIdCGoogleCalendar();
@@ -48,11 +42,15 @@ function applyNNotionToCGoogleCalendar() {
   for (let i = 0; i < notionChBuffer.length; ++i) {
     if (idCGoogleCalendar.includes(notionChBuffer[i].GoogleCalendarID)) {
       //this is a existing one
-      gci.updateEvent(notionChBuffer[i].GoogleCalendarID, notionChBuffer[i]); 
+      gci.updateEvent(notionChBuffer[i].GoogleCalendarID, notionChBuffer[i]);
     } else {
       // cGoogleCalendar.push(notionChBuffer[i]);
-      cNotion[getCNotionIndexById(notionChBuffer[i].NotionID)].GoogleCalendarID = gci.addEvent(notionChBuffer[i]);
-      cGoogleCalendar.push(cNotion[getCNotionIndexById(notionChBuffer[i].NotionID)]);
+      cNotion[
+        getCNotionIndexById(notionChBuffer[i].NotionID)
+      ].GoogleCalendarID = gci.addEvent(notionChBuffer[i]);
+      cGoogleCalendar.push(
+        cNotion[getCNotionIndexById(notionChBuffer[i].NotionID)]
+      );
     }
   }
   //checking left overs for deletion
@@ -69,38 +67,26 @@ function getCNotionIndexById(id: string): any {
 
 function getCGoogleCalendarById(id: string) {}
 
-function generateIdCNotion() {
-  idCNotion = [];
-  for (var i = 0; i < ((cNotion == undefined)? 0:cNotion.length); ++i) {
-    idCNotion.push(cNotion[i].NotionID);
-  }
-}
 
-function generateIdCGoogleCalendar() {
-  idCGoogleCalendar = [];
-  for (var i = 0; i < cGoogleCalendar.length; ++i) {
-    idCGoogleCalendar.push(cGoogleCalendar[i].GoogleCalendarID);
-  }
-}
 
-function removeLeftoverCNotion() {}
 
-async function init() {
-  ni = new NotionIntegration();
-  gci = new GoogleCalendarIntegration();
-  await gci.init();
-  
-  notionChBuffer = [];
-  cGoogleCalendar = [];
-  nGoogleCalendar =[];
-  cNotion = [];
-  nNotion = [];
 
-  initCNotion();
-  initCGoogleCalendar();
+// async function init() {
+//   ni = new NotionIntegration();
+//   gci = new GoogleCalendarIntegration();
+//   await gci.init();
 
-  generateIdCNotion();
-  generateIdCGoogleCalendar();
+//   notionChBuffer = [];
+//   cGoogleCalendar = [];
+//   nGoogleCalendar = [];
+//   cNotion = [];
+//   nNotion = [];
+
+//   initCNotion();
+//   initCGoogleCalendar();
+
+//   generateIdCNotion();
+//   generateIdCGoogleCalendar();
 }
 
 async function serverPreparation() {
@@ -115,77 +101,11 @@ function startServer() {
 }
 
 async function nicLoop() {
-   
-  generateIdCNotion();//generate new check up ids
+  generateIdCNotion(); //generate new check up ids
   notionChBuffer = []; //removing the previous changes
   nNotion = ni.transformInActions(await ni.getActions()); //getting the new Notion status
   extractNotionChanges(); //getting the changes(comparing with old status), left over ids are all ids of unknown elements
   applyNNotionToCGoogleCalendar(); //applying all new things to google calendar
- updateCGoogleCalendarJSONData(); 
-  await updateCNotionJSONData();//writing it into json
-  console.log("hundling!")
-}
-
-function extractNotionChanges() {
-  for (var i = 0; i < nNotion.length; ++i) {
-    if (idCNotion.includes(nNotion[i].NotionID)) {
-      if (getCNotionIndexById(nNotion[i].NotionID) != undefined && nNotion[i] != cNotion[getCNotionIndexById(nNotion[i].NotionID)]) {
-        notionChBuffer.push(nNotion[i]);
-      }
-      for (let x = 0; x < idCNotion.length; ++x) {
-        if (idCNotion[x] == nNotion[i].NotionID) {
-          delete idCNotion[x];
-        }
-      }
-    } else {
-      notionChBuffer.push(nNotion[i]);
-    }
-  }
-  cNotion = nNotion;
-}
-
-function extractDifferences(berforeC: any[], afterC: any[]): any[] {
-  var sortedB: any[] = berforeC.sort();
-  var sortedA: any[] = afterC.sort();
-  if (sortedB.length >= sortedA.length) {
-    for (var i = 0; i < sortedB.length; i++) {
-      if (sortedA[i] != sortedB[i]) {
-      }
-    }
-  } else {
-    for (var i = 0; i < sortedB.length; i++) {
-      // if(sortedA[i] == sortedB[i])
-    }
-  }
-
-  return [];
-}
-
-
-
-
-async function updateCNotionJSONData() {
-  const jsonString = JSON.stringify(cNotion==undefined?[]:cNotion);
-  await fs.writeFile(
-    path.resolve(__dirname, "./database/notion.json"),
-    jsonString,
-    (err: any) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("success");
-      }
-    }
-  );
-}
-
-function updateCGoogleCalendarJSONData() {
-  const jsonString = JSON.stringify(cGoogleCalendar==undefined?[]:cGoogleCalendar);
-  // const jsonString = "lesgo";
-  console.log(jsonString)
-  fs.writeFileSync(
-    path.resolve(__dirname, "./database/googleCalendar.json"),
-    jsonString,
-    'utf8'
-  );
+  updateCGoogleCalendarJSONData();
+  await updateCNotionJSONData(); //writing it into json
 }
