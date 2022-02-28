1b288e9b63de4259b9f25e282e0d3ceeddcf760f
ungarmichael <ungarmichael.mail@gmail.com>
2022-02-28 20:58:56 +0100

    fix: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // import { fetchFromJournal } from "./controllers/notion";
 // // fetchFromJournal();
 
-const { Client } = require('@notionhq/client');
+const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_ACCESS_TOKEN });
 
 (async () => {
-  const databaseId = process.env.NOTION_ACTION_DATABASE_ID;
+  const databaseId = "044b33e9-fda4-4c9b-b943-e0750fa4931a";
   const response = await notion.databases.retrieve({ database_id: databaseId });
   console.log(response);
 })();
--- src/validations/JournalValidation.ts
+++
@@ -1,4 +0,0 @@
-export const checkOnMissingEntry = (): boolean => {
-  return false;
-};
-
