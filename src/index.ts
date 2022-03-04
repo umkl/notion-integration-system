1d70dc8aa129964ba2a91ff8d4fc1efdef38a8fa
ungarmichael <ungarmichael.mail@gmail.com>
2022-03-04 21:29:39 +0100

    -

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import { fetchFromJournal } from "./controllers/notion";
 // fetchFromJournal();
-
+import { Client } from "@notionhq/client";
 
 require("dotenv").config();
 
@@ -8,7 +8,8 @@ const notion = new Client({
   auth: process.env.NOTION_ACCESS_TOKEN,
 });
 
-(async ()=>{
-  const res = await notion.databases.retrieve()
-  console.log(res)//dsd
-})
+(async () => {
+  const databaseId = process.env.NOTION_ACTION_DATABASE_ID!;
+  const res = await notion.databases.retrieve({ database_id: databaseId });
+  console.log(res); //dsd
+})();
