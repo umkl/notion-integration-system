dd10a6586b2708c406e40f77f3d4c0d22f3c9bde
ungarmichael <ungarmichael.mail@gmail.com>
2022-03-04 08:21:28 +0100

    tsc setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-// import { fetchFromJournal } from "./controllers/notion";
-// // fetchFromJournal();
-require("dotenv").config();
+// // import { fetchFromJournal } from "./controllers/notion";
+// // // fetchFromJournal();
+// require("dotenv").config();
 
-const { Client } = require("@notionhq/client");
+// const { Client } = require("@notionhq/client");
 
-const notion = new Client({
-  auth: process.env.NOTION_ACCESS_TOKEN,
-});
+// const notion = new Client({
+//   auth: process.env.NOTION_ACCESS_TOKEN,
+// });
 
-(async () => {
-  const databaseId = "044b33e9-fda4-4c9b-b943-e0750fa4931a";
-  const response = await notion.databases.retrieve({ database_id: databaseId });
-  console.log(response);
-})();
+// (async () => {
+//   const databaseId = "044b33e9-fda4-4c9b-b943-e0750fa4931a";
+//   const response = await notion.databases.retrieve({ database_id: databaseId });
+//   console.log(response);
+// })();
+console.log("abfahrt")
